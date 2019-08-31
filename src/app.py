f0f32e1cd5a7c70b208655084be258b35a3cfed9
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-08-31 17:18:56 -0300

    Añado dos endopints simples

--- src/app.py
+++ src/app.py
@@ -1,10 +1,53 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello World!"
+
+
+@app.route('/places')
+def places():
+    places_list = [
+        {
+            "id": "1",
+            "name": "Mac",
+            "coordinates": {
+                "latitude": 1,
+                "longitude": 2
+            }
+        },
+        {
+            "id": "2",
+            "name": "Burger",
+            "coordinates": {
+                "latitude": 1,
+                "longitude": 2
+            }
+        }
+    ]
+
+    return jsonify(places_list)
+
+
+@app.route('/users')
+def users():
+    users_list = [
+        {
+            "id": "1",
+            "name": "Pepe Argento"
+        },
+        {
+            "id": "2",
+            "name": "Moni Potrelli"
+        }
+    ]
+
+    return jsonify(users_list)
+
 
 if __name__ == '__main__':
     app.run()
