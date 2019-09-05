8bf9a27346fc4985bb4bfeed983ba295eb8b82ac
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2019-09-04 22:38:28 -0300

    algunos cambios al endpoint

--- src/app.py
+++ src/app.py
@@ -6,16 +6,16 @@ from models.Coordinates import Coordinates
 
 APP = Flask(__name__)
 
+db = DatabaseApi('localhost:27017')
+
+#   Endpoint de testing de santiago-alvarezjulia
 @APP.route('/test')
 def test():
-    
-    db = DatabaseApi('localhost:27017')
-    #document = DeliveryDisponible('1', 'cacho', 'url_imagen.com', Coordinates(0, 0))
-
     doc = {'_id': '1', 'name': 'santo'}
     db.agregar_documento('deliveries_disponibles', doc)
 
     return db.encontrar_documento('deliveries_disponibles', doc)
+
 #
 #   Endpoint Rest API: Deliveries Disponibles
 #
@@ -27,13 +27,17 @@ def test():
 def deliveries_disponibles():
     if request.method == 'POST':
         # agregar delivery como disponible
-        return {'status': 201, 'body': 'Created Succesfully'}
+        return db.agregar_documento('deliveries_disponibles', request.json)
     elif request.method == 'GET':
         # get deliveries disponibles cercanos
-        return {'status': 200, 'body': []}
+        query = {'coordinates': {'$within': {'$center': [[0, 0], 5]}}}
+        return db.encontrar_lista_documentos('deliveries_disponibles', query)
     elif request.method == 'DELETE':
         # eliminar delivery como disponible
         return {'status': 200, 'body': 'Deleted Succesfully'}
+    else:
+        # Bad Request
+        return {'status': 400, 'body': 'Bad Request'}
 
 
 if __name__ == '__main__':
