b2563dafa361288d235d39f7156b60b89e02d159
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-09-07 19:19:28 -0300

    Añado validaciones al crear un usuario

--- src/controllers/users_controller.py
+++ src/controllers/users_controller.py
@@ -1,25 +1,36 @@
+from bson import ObjectId
+
 from repositories.mongo_client import client
 from flask import jsonify
 from flask import Blueprint
 from flask import request
 
+from schemas import user_schemas
+
 users_blueprint = Blueprint('users', __name__)
+
+
+@users_blueprint.route('/<_id>', methods=['GET'])
+def get_user(_id):
+    user = client.foodie.users.find_one({'_id': ObjectId(_id)})
+    return jsonify(user)
 
 
 @users_blueprint.route('/', methods=['GET'])
 def get():
     users = client.foodie.users.find()
-    response = {"users": [{
-        "id": user["_id"],
-        "name": user["name"]
-    } for user in users]}
 
-    return jsonify(response)
+    return jsonify({"users": [user for user in users]})
 
 
 @users_blueprint.route('/', methods=['POST'])
 def post():
-    name = request.json['name']
-    client.foodie.users.insert_one({'name': name})
-    return "ok"
+    content = request.get_json()
+    schema = user_schemas.UserSchema()
+    user_data = schema.load(content)
 
+    return jsonify(
+        {
+            "_id": client.foodie.users.insert_one(user_data).inserted_id
+        }
+    )
