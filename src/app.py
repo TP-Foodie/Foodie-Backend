ba9989b8e2f1e51857854b8c6f830f18898e54eb
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-09-08 19:53:05 -0300

    Añado cors

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from encoders import CustomJSONEncoder
+from flask_cors import CORS
 from controllers.places_controller import PLACES_BLUEPRINT
 from controllers.users_controller import USERS_BLUEPRINT
 
 APP = Flask(__name__)
+CORS(APP)
 
 APP.json_encoder = CustomJSONEncoder
 APP.register_blueprint(PLACES_BLUEPRINT, url_prefix='/places')
