8ad1dbc9eda6185bf4114683f0b48c779bc75419
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2019-09-10 12:36:30 -0300

    corregido bug OR en py

--- src/deliveries_disponibles/schemas/extra_validations.py
+++ src/deliveries_disponibles/schemas/extra_validations.py
@@ -7,7 +7,7 @@ def validate_coordinates(coordinates):
     print(coordinates)
     if (len(coordinates) != 2):
         raise ValidationError("Coordinates lenght must be 2.")
-    if (coordinates[0] > 180 | coordinates[0] < -180):
+    if (coordinates[0] > 180 or coordinates[0] < -180):
         raise ValidationError("Valid longitude values are between -180 and 180, both inclusive.")
-    if (coordinates[1] > 90 | coordinates[1] < -90):
+    if (coordinates[1] > 90 or coordinates[1] < -90):
         raise ValidationError("Valid latitude values are between -90 and 90, both inclusive.")
