294436c0eedc25b1f8e298cb6f957983ce91a4cd
santiago-alvarezjulia <santiago.alvarezjulia@gmail.com>
2019-09-11 10:44:45 -0300

    agrego test de extra_validations

--- src/deliveries_disponibles/schemas/extra_validations.py
+++ src/deliveries_disponibles/schemas/extra_validations.py
@@ -7,9 +7,12 @@ from marshmallow import ValidationError
 # Valid latitude values are between -90 and 90, both inclusive.
 def validate_coordinates(coordinates):
     """ This method validates geospatial coordinates """
+    if coordinates is None:
+        raise ValidationError("Coordinates is None.")
     if len(coordinates) != 2:
         raise ValidationError("Coordinates lenght must be 2.")
     if (coordinates[0] > 180 or coordinates[0] < -180):
         raise ValidationError("Valid longitude values are between -180 and 180, both inclusive.")
     if (coordinates[1] > 90 or coordinates[1] < -90):
         raise ValidationError("Valid latitude values are between -90 and 90, both inclusive.")
+    return True
