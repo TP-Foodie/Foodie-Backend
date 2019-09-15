2609b8f9dd354afdadff3fb10e687433e0628819
Facundo Gerstner <fgerstner@devartis.com>
2019-09-15 11:42:47 -0300

    add status field

--- src/models/order.py
+++ src/models/order.py
@@ -1,7 +1,6 @@
-from mongoengine import Document, IntField
+from mongoengine import Document, IntField, StringField
 
 
 class Order(Document):
     number = IntField
-
-
+    status = StringField(max_length=10)
