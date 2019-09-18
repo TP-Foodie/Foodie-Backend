258c8c95835448ca0a1ae807b09b74174a24f241
Facu G <fgerstner@devartis.com>
2019-09-18 12:46:29 -0300

    use schemas instances

--- src/controllers/order_controller.py
+++ src/controllers/order_controller.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, jsonify
 
 from src.repositories import order_repository
-from src.schemas.order import orders_schema, order_schema
+from src.schemas.order import ListOrderSchema, DetailsOrderSchema
 
 ORDERS_BLUEPRINT = Blueprint('orders', 'order_controller', url_prefix='/orders')
 
 
 @ORDERS_BLUEPRINT.route('/', methods=['GET'])
 def list_orders():
-    data = orders_schema.dump(order_repository.list_all())
+    data = ListOrderSchema(many=True).dump(order_repository.list_all())
     return jsonify(data)
 
 
 @ORDERS_BLUEPRINT.route('/<order_id>', methods=['GET'])
 def order_details(order_id):
-    data = order_schema.dump(order_repository.get_order(order_id))
+    data = DetailsOrderSchema().dump(order_repository.get_order(order_id))
     return jsonify(data)
--- src/schemas/order.py
+++ src/schemas/order.py
@@ -8,14 +8,9 @@ class ListOrderSchema(Schema):
         fields = ('id', 'number', 'status', 'type')
 
 
-orders_schema = ListOrderSchema(many=True)
-
-
 class DetailsOrderSchema(Schema):
     owner = fields.Nested(UserSchema)
 
     class Meta:
         fields = ('id', 'number', 'status', 'type', 'owner')
 
-
-order_schema = DetailsOrderSchema()
