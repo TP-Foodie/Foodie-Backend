3c8e056ca2081434b22d989a83f40483465071d9
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-09-24 17:27:09 -0300

    Muevo de lugar los archivos para homogeneizar código

--- test/encoders_test.py
+++ test/encoders_test.py
@@ -7,24 +7,25 @@ from src.encoders import CustomJSONEncoder
 ENCODER = CustomJSONEncoder()
 
 
-def test_datetime_json_encode():
-    """
-    Test to ensure that the default behaviour is still valid
-    """
+class TestEncoder:
 
-    expected = '"Sat, 31 Aug 2019 20:58:53 GMT"'
-    date = datetime.datetime(2019, 8, 31, 20, 58, 53, 391516)
+    def test_datetime_json_encode(self):
+        """
+        Test to ensure that the default behaviour is still valid
+        """
 
-    assert expected == ENCODER.encode(date)
+        expected = '"Sat, 31 Aug 2019 20:58:53 GMT"'
+        date = datetime.datetime(2019, 8, 31, 20, 58, 53, 391516)
 
+        assert expected == ENCODER.encode(date)
 
-def test_mongo_document():
-    """
-    Test to ensure mongo document model works
-    """
-    expected = {"name": "jose"}
-    document = create_autospec(Document)
+    def test_mongo_document(self):
+        """
+        Test to ensure mongo document model works
+        """
+        expected = {"name": "jose"}
+        document = create_autospec(Document)
 
-    document._data = {"name": "jose"}  # pylint: disable=W0212
+        document._data = {"name": "jose"}  # pylint: disable=W0212
 
-    assert ENCODER.encode(expected) == ENCODER.encode(document)
+        assert ENCODER.encode(expected) == ENCODER.encode(document)
