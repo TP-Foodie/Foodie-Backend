361eaac5807579715e2417ced058bcb098d99c7a
Mariano Hielpos <mariano.hielpos@despegar.com>
2019-09-25 20:06:56 -0300

    Añado test para jwt service

---
+++ test/services/jwt_service_test.py
@@ -0,0 +1,17 @@
+from unittest import mock
+
+
+class TestJwtService:
+
+    @mock.patch('src.settings.Config')
+    def test_jwt_service(self, config):
+        config.JWT_SECRET.return_value = 'jwt_secret'
+        data = {
+            'email': 'user@mail.com',
+            'password': 'password'
+        }
+
+        from src.services import jwt_service
+        encoded_data = jwt_service.encode_data_to_jwt(data)
+
+        assert jwt_service.decode_jwt_data(encoded_data) == data
