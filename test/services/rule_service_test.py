39636c25bbf4ae94d25a3b645d9e871ab91cfc33
Facundo Gerstner <fgerstner@devartis.com>
2019-09-29 16:33:10 -0300

    remove comments

--- test/services/rule_service_test.py
+++ test/services/rule_service_test.py
@@ -1,23 +1,3 @@
-# {
-#     'id': 1,
-#     'condition': {
-#         'id': 1,
-#         'variable': 'today',
-#         'operator': 'IS',
-#         'condition_value': 'miercoles',
-#         'condition': {
-#             'id': 1,
-#             'variable': 'time',
-#             'type': 'GTE'
-#         }
-#     },
-#     'consequence': {
-#         'id': 1,
-#         'type': '',
-#         'value': -5
-#     },
-#     fact: 'jueves'
-# }
 import pytest
 
 from src.models.rule import RuleCondition, Rule, RuleConsequence
