643f21a2e70ac32d24b7c6c4d385cfdb611aa3e2
Facundo Gerstner <fgerstner@devartis.com>
2019-09-29 14:21:14 -0300

    add variables

--- src/models/rule.py
+++ src/models/rule.py
@@ -2,4 +2,42 @@ from mongoengine import Document, fields
 
 
 class RuleCondition(Document):
-    variable = fields.StringField(max_length=10)
+    UserReputation = 'UR'
+    UserDailyTravels = 'UDT'
+    UserMonthlyTravels = 'UMT'
+    UserAntiquity = 'UA'
+    DeliveryReputation = 'DR'
+    DeliveryDailyTravels = 'DDT',
+    DeliveryMonthlyTravels = 'DMT',
+    DeliveryAntiquity = 'DA',
+    DeliveryBalance = 'DB',
+    CashPaymentMethod = 'CPM',
+    CreditPaymentMethod = 'CRPM'
+    OrderDuration = 'OD',
+    OrderDistance = 'ODI',
+    OrderPosition = 'OP',
+    OrderTime = 'OT',
+    OrdersQuantity = 'OC',
+    TravelDay = 'TD'
+
+    VARIABLES = (
+        UserReputation,
+        UserDailyTravels,
+        UserMonthlyTravels,
+        UserAntiquity,
+        DeliveryReputation,
+        DeliveryDailyTravels,
+        DeliveryMonthlyTravels,
+        DeliveryAntiquity,
+        DeliveryBalance,
+        CashPaymentMethod,
+        CreditPaymentMethod,
+        OrderDuration,
+        OrderDistance,
+        OrderPosition,
+        OrderTime,
+        OrdersQuantity,
+        TravelDay,
+    )
+
+    variable = fields.StringField(max_length=10, choices=VARIABLES)
--- test/models/rule_tests.py
+++ test/models/rule_tests.py
@@ -8,3 +8,4 @@ class TestCondition:
     @pytest.mark.parametrize('attr_name', ['variable'])
     def test_has_attribute(self, attr_name, a_condition):
         assert_attr_exists(a_condition, attr_name)
+
