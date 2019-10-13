9ca44bac8bc09293450fea1454fa88e879605871
Facundo Gerstner <fgerstner@devartis.com>
2019-10-13 18:21:10 -0300

    add equality test

--- test/services/operator_service_test.py
+++ test/services/operator_service_test.py
@@ -10,3 +10,6 @@ class TestOperatorService:
 
     def test_apply_greater_than_retunrs_false_if_its_smaller(self):
         assert not self.operator_service.apply(RuleCondition.GREATER_THAN, 1, 3)
+
+    def test_apply_greater_than_returns_false_if_are_equal(self):
+        assert not self.operator_service.apply(RuleCondition.GREATER_THAN, 1, 1)
