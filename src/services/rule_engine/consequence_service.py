6845f0b704007784c12708d9535946162141b5b1
Facundo Gerstner <fgerstner@devartis.com>
2019-10-15 18:53:35 -0300

    add logic for percentage consequence type

--- src/services/rule_engine/consequence_service.py
+++ src/services/rule_engine/consequence_service.py
@@ -1,3 +1,24 @@
-class RuleConsequenceService:
-    def apply(self, consequence, value, value_to_apply):
+from src.models.rule import RuleConsequence
+
+
+class ValueConsequenceType:
+    def apply(self, value, value_to_apply):
         return value + value_to_apply
+
+
+class PercentageConsequenceType:
+    def to_percentage(self, value):
+        return value / 100
+
+    def apply(self, value, value_to_apply):
+        return value + self.to_percentage(value * value_to_apply)
+
+
+class RuleConsequenceService:
+    CONSEQUENCE_TYPE_MAPPING = {
+        RuleConsequence.VALUE: ValueConsequenceType,
+        RuleConsequence.PERCENTAGE: PercentageConsequenceType
+    }
+
+    def apply(self, consequence, value, value_to_apply):
+        return self.CONSEQUENCE_TYPE_MAPPING[consequence]().apply(value, value_to_apply)
