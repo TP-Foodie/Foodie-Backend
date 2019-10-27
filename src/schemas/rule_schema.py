251cc2bf26ae974f8279ce2d392267e91d421cb8
azowyl <facugerstner_29@hotmail.com>
2019-10-27 14:40:37 -0300

    fix tests

--- src/schemas/rule_schema.py
+++ src/schemas/rule_schema.py
@@ -8,7 +8,7 @@ class ConditionSchema(Schema):
 
 class ConsequenceSchema(Schema):
     class Meta:
-        fields = ('consequence_type', 'value', 'unit_value')
+        fields = ('consequence_type', 'value', 'variable')
 
 
 class ListRuleSchema(Schema):
