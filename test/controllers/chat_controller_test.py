3ac4ea7f193e7bd29ea6052b01647344d6c69fde
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2019-10-31 17:50:03 -0300

    listo chat

--- test/controllers/chat_controller_test.py
+++ test/controllers/chat_controller_test.py
@@ -31,9 +31,9 @@ class TestChatController(TestMixin):
                 'timestamp': timestamp
             })
 
-    def get_chat(self, client, a_chat, a_client_user):
+    def get_chat(self, client, id_chat, a_client_user):
         self.login(client, a_client_user.email, a_client_user.password)
-        return self.get(client, self.build_url('/chats/{}'.format(str(a_chat.id))))
+        return self.get(client, self.build_url('/chats/{}'.format(id_chat)))
 
     def get_chat_messages(self, client, a_client_user, id_chat, page, limit):
         self.login(client, a_client_user.email, a_client_user.password)
@@ -44,24 +44,29 @@ class TestChatController(TestMixin):
             limit
         )
 
-    def test_chats_endpoint_exists(self, a_client, a_chat, a_client_user):
-        response = self.get_chat(a_client, a_chat, a_client_user)
-        assert_200(response)
+    def test_chats_endpoint_exists(self, a_client, a_client_user):
+        response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        assert_201(response)
 
     def test_get_chat_for_unauthenticated(self, a_client, a_chat):
         response = a_client.get(self.build_url('/chats/{}'.format(str(a_chat.id))))
         assert_401(response)
 
-    def test_get_chat(self, a_client, a_chat, a_client_user):
-        response = self.get_chat(a_client, a_chat, a_client_user)
+    def test_get_chat_for_not_chat_member(self, a_client, a_client_user, a_chat):
+        response = self.get_chat(a_client, a_chat.id, a_client_user)
+        assert_401(response)
+
+    def test_get_chat(self, a_client, a_client_user):
+        chat_response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        chat = json.loads(chat_response.data)
+        response = self.get_chat(a_client, str(chat["id"]), a_client_user)
         assert_200(response)
-        chat = json.loads(response.data)
 
         assert chat == {
-            'id': str(a_chat.id),
-            'uid_1': a_chat.uid_1,
-            'uid_2': a_chat.uid_2,
-            'id_order': a_chat.id_order,
+            'id': str(chat["id"]),
+            'uid_1': str(a_client_user.id),
+            'uid_2': "b",
+            'id_order': "c",
         }
 
     def test_create_chat_for_unauthenticated(self, a_client):
@@ -72,15 +77,17 @@ class TestChatController(TestMixin):
         assert_401(response)
 
     def test_user_should_be_able_to_create_chat(self, a_client, a_client_user):
-        response = self.create_chat(a_client, a_client_user, "a", "b", "c")
+        response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
         assert_201(response)
 
     def test_create_chat_should_create_one_on_db(self, a_client, a_client_user):
-        self.create_chat(a_client, a_client_user, "a", "b", "c")
+        self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
         assert len(chat_repository.list_all()) == 1
 
-    def test_chat_messages_endpoint_exists(self, a_client, a_chat, a_client_user):
-        response = self.get_chat_messages(a_client, a_client_user, str(a_chat.id), 0, 50)
+    def test_chat_messages_endpoint_exists(self, a_client, a_client_user):
+        chat_response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        chat = json.loads(chat_response.data)
+        response = self.get_chat_messages(a_client, a_client_user, str(chat["id"]), 0, 50)
         assert_200(response)
 
     def test_create_chat_message_for_unauthenticated(self, a_client, a_chat):
@@ -94,32 +101,42 @@ class TestChatController(TestMixin):
         response = a_client.get('api/v1/chats/{}/messages/'.format(str(a_chat.id)))
         assert_401(response)
 
-    def test_create_chat_message_should_create_one_on_db(self, a_client, a_client_user, a_chat):
-        self.create_chat_message(a_client, a_client_user, str(a_chat.id), "a", "b", 0.0)
-        assert chat_repository.count_chat_messages(str(a_chat.id)) == 1
+    def test_create_chat_message_should_create_one_on_db(self, a_client, a_client_user):
+        chat_response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        chat = json.loads(chat_response.data)
+        self.create_chat_message(a_client, a_client_user, str(chat["id"]), "a", "b", 0.0)
+        assert chat_repository.count_chat_messages(str(chat["id"])) == 1
 
-    def test_list_chat_messages(self, a_client, a_client_user, a_chat_message_from_uid_1):
+    def test_list_chat_messages(self, a_client, a_client_user):
+        chat_response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        chat = json.loads(chat_response.data)
+        self.create_chat_message(
+            a_client, a_client_user, str(chat["id"]), str(a_client_user.id), "b", 0.0
+        )
         response = self.get_chat_messages(
-            a_client, a_client_user, a_chat_message_from_uid_1.id_chat, 0, 50
+            a_client, a_client_user, str(chat["id"]), 0, 50
         )
         chat_message = json.loads(response.data)["messages"][0]
 
-        assert chat_message == {
-            'id': str(a_chat_message_from_uid_1.id),
-            'uid_sender': a_chat_message_from_uid_1.uid_sender,
-            'message': a_chat_message_from_uid_1.message,
-            'timestamp': a_chat_message_from_uid_1.timestamp,
-            'id_chat': a_chat_message_from_uid_1.id_chat
-        }
+        assert chat_message['uid_sender'] == str(a_client_user.id)
+        assert chat_message['message'] == "b"
+        assert chat_message['timestamp'] == 0.0
+        assert chat_message['id_chat'] == str(chat["id"])
 
     def test_list_chat_messages_paging(self, a_client, a_client_user):
-        self.create_chat_message(a_client, a_client_user, "id_chat", "a", "1", 1.0)
-        self.create_chat_message(a_client, a_client_user, "id_chat", "a", "2", 2.0)
+        chat_response = self.create_chat(a_client, a_client_user, str(a_client_user.id), "b", "c")
+        chat = json.loads(chat_response.data)
+        self.create_chat_message(
+            a_client, a_client_user, str(chat["id"]), str(a_client_user.id), "1", 1.0
+        )
+        self.create_chat_message(
+            a_client, a_client_user, str(chat["id"]), str(a_client_user.id), "2", 2.0
+        )
 
-        assert chat_repository.count_chat_messages("id_chat") == 2
+        assert chat_repository.count_chat_messages(str(chat["id"])) == 2
 
         response = self.get_chat_messages(
-            a_client, a_client_user, "id_chat", 1, 1
+            a_client, a_client_user, str(chat["id"]), 1, 1
         )
         chat_message = json.loads(response.data)["messages"][0]
 
