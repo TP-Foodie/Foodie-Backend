4fbea08c65df7f863b46d134c27eb51c81b6d2f3
azowyl <facugerstner_29@hotmail.com>
2019-11-17 15:49:22 -0300

    add test for rating

--- test/models/user_rating_test.py
+++ test/models/user_rating_test.py
@@ -1,4 +1,5 @@
 import pytest
+from mongoengine import ValidationError
 
 from test.support.utils import assert_attr_exists
 
@@ -8,3 +9,9 @@ class TestUserRating:
     @pytest.mark.parametrize('attr_name', ['user', 'description', 'rating'])
     def test_has_attribute(self, attr_name, a_user_rating):
         assert_attr_exists(a_user_rating, attr_name)
+
+    def test_rating_cant_be_greater_than_5(self, a_user_rating):
+        a_user_rating.rating = 10
+
+        with pytest.raises(ValidationError):
+            a_user_rating.save()
